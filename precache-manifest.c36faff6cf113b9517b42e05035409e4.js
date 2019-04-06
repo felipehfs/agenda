ba75a1cb2ea58c40dcc5487a467d1b77a502f352
Felipe Henrique Ferreira sousa <felipehfsouza@gmail.com>
2019-04-06 11:59:30 -0300

    Updates

---
+++ precache-manifest.c36faff6cf113b9517b42e05035409e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a722482b8e7541d142a8",
+    "url": "/agenda/static/js/runtime~main.67f6c2e7.js"
+  },
+  {
+    "revision": "1642168d999f83fc0a38",
+    "url": "/agenda/static/js/main.0c14b25c.chunk.js"
+  },
+  {
+    "revision": "7ab227d5a33b7fed0f44",
+    "url": "/agenda/static/js/2.e13d70e8.chunk.js"
+  },
+  {
+    "revision": "1642168d999f83fc0a38",
+    "url": "/agenda/static/css/main.4e161ddf.chunk.css"
+  },
+  {
+    "revision": "a22d6ac948d3c1fb06815f4eea6f2bbd",
+    "url": "/agenda/index.html"
+  }
+];
